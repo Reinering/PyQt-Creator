0aa25808880e4f9382897162f8224cf6fb950633
Reinering <nbxlhc@hotmail.com>
2024-09-27 12:41:26 +0800

    update console ui

--- ui/Ui_ConsoleWidget.py
+++ ui/Ui_ConsoleWidget.py
@@ -15,33 +15,21 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
 
-from qfluentwidgets import ScrollArea
+from qfluentwidgets import (SingleDirectionScrollArea, TabBar)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(506, 488)
+        Form.resize(706, 488)
         font = QFont()
         font.setPointSize(10)
         Form.setFont(font)
-        self.gridLayout = QGridLayout(Form)
-        self.gridLayout.setSpacing(1)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.gridLayout.setContentsMargins(1, 1, 1, 1)
-        self.ScrollArea = ScrollArea(Form)
-        self.ScrollArea.setObjectName(u"ScrollArea")
-        self.ScrollArea.setFont(font)
-        self.ScrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 502, 484))
-        self.ScrollArea.setWidget(self.scrollAreaWidgetContents)
-
-        self.gridLayout.addWidget(self.ScrollArea, 0, 0, 1, 1)
-
+        self.TabBar = TabBar(Form)
+        self.TabBar.setObjectName(u"TabBar")
+        self.TabBar.setGeometry(QRect(20, 20, 540, 46))
 
         self.retranslateUi(Form)
 
